6be984cba6fa375dff6f604ff2b1235b0cdb3ab4
Michael Vaughan <mgvaughan66@gmail.com>
2023-01-20 13:41:44 -0500

    about page margin changes

--- src/scenes/about/index.jsx
+++ src/scenes/about/index.jsx
@@ -19,7 +19,7 @@ const Link = ({ page, icon, selectedPage, setSelectedPage }) => {
 
 const About = ({ selectedPage, setSelectedPage }) => {
   return (
-    <div id='about' className="w-full text-center lg:h-screen ">
+    <div id='about' className="w-full text-center lg:h-screen pt-24 pb-32">
       <div className="w-full h-full mx-auto p-2 flex justify-center items-center">
         <div className="">
           <p className="text-green  max-w-[70%] m-auto text-4xl font-bold">
