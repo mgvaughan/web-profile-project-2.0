3d74afe7853566e16b214b8db1f79a33f0c44c17
Michael Vaughan <mgvaughan66@gmail.com>
2023-01-22 16:14:55 -0500

    updating project description lengths

--- src/scenes/projects/index.jsx
+++ src/scenes/projects/index.jsx
@@ -44,20 +44,20 @@ const Projects = () => {
             thumbnail={mernAdminDashImg}
             cardTitle="Fullstack Admin Dashboard"
             description="The link below is to an example of a fullstack admin dashboard I created using the MERN stack. This was a good project for me to get experience building because it gave me an opportunity to build a realistic a fullstack site with realistic data, and using the Redux library.  Website was deployed from using Render."
-            skills="MongoDB, React, Redux, Express, Node.js, JavaScript, CSS"
+            skills="MongoDB, React, Redux, Express, Node.js, JavaScript"
             url="https://mern-fullstack-admin-dashboard.onrender.com"
           />
           <ProjectCard
             thumbnail={fitnessAppImg}
-            cardTitle="Gym Website Using React and Typescript"
-            description="The link below is to an example of a gym's website I created using React and Typescript. This was a good project for me to get experience building because it gave me an opportunity to build a realistic company website, incorporating animations, typescript, and tailwindcss.  Also gained experience using react-hook-form, and with deploying the website from cloudflare."
+            cardTitle="Gym Website Using React/Typescript"
+            description="The link below is to an example of a gym's website I created using React and Typescript. This was a good project for me to get experience building because it gave me an opportunity to build a realistic company website incorporating animations, and tailwindcss.  I also gained experience using react-hook-form, and with deploying from cloudflare."
             skills="React, Typescript, JavaScript, CSS, HTML"
             url="https://32cf7530.typescript-react-fitness-application.pages.dev/"
           />
           <ProjectCard
             thumbnail={reactPortfolioImg}
             cardTitle="Example Portfolio Website Using React"
-            description="The link below is to an example of a portfolio website I created using the MERN stack. In this project, I got to continue to practice using React while gaining more experience using darker styling choices and animations to build a pretty cool looking example portfolio website."
+            description="The link below is to an example of a portfolio website I created using React and Tailwind. In this project, I got to continue to practice using React while gaining more experience using darker styling choices and animations to build a pretty cool looking example portfolio website."
             skills="React, JavaScript, CSS, HTML"
             url="https://react-portfolio-app-mv.netlify.app/"
           />
@@ -65,27 +65,27 @@ const Projects = () => {
             thumbnail={mernSocialMediaImg}
             cardTitle="MERN Fullstack Social Media App"
             description="The link below is to an example of a social media website I created using the MERN stack. In this project, I got experience using Mongo as well as deploying the site using Render. I also continued to increase my experience with Authentication/Authorization, React, Node.js and Tailwind stylings."
-            skills="Mongo, Express, React, Node.js, JavaScript, CSS, HTML"
+            skills="Mongo, Express, React, Node.js, JavaScript, CSS"
             url="https://fullstack-social-media-website.onrender.com/"
           />
           <ProjectCard
             thumbnail={adminDashImg}
             cardTitle="Admin Dashboard Using React"
-            description="The link below is to an example of an admin dashboard I created using React. This project was a lot of fun to create because it really helped me to gain understanding on higher level usage of React library, as well as give me experience incorporating light and dark modes, as well as with using Material UI, Nivo Charts, Formik, Yup, Full Calendar, and Data Grid."
+            description="The link below is to an example of an admin dashboard I created using React. This project was a lot of fun to create because it really helped me to gain understanding on higher level usage of React library, as well as give me experience incorporating light/dark modes, as well as with using Material UI, Nivo Charts, Formik, Yup, Full Calendar, and Data Grid."
             skills="React, JavaScript, CSS, HTML"
             url="https://react-admin-dashboard-mv.netlify.app/"
           />
           <ProjectCard
             thumbnail={reactTetrisImg}
             cardTitle="Tetris Game Using React"
-            description="The link below is to a version of the Tetris game that I created using React. This project was a lot of fun to create because it really helped me to gain understanding on how to better utilize React components to create a relatively complex program.  I find building games are a great way to challenge your problem solving skills while developing. Use up arrow to rotate!"
+            description="The link below is to a version of Tetris that I created using React. This project was a lot of fun to create because it really helped me to gain understanding utilizing React components to create a relatively complex program.  I find building games are a great way to challenge your problem solving skills while developing. Use up arrow to rotate!"
             skills="React, Node.js/Express, JavaScript, CSS, HTML"
             url="https://react-tetris-mv.herokuapp.com/"
           />
           <ProjectCard
             thumbnail={javascriptTetrisImg}
             cardTitle="Tetris Game Using Javascript"
-            description="The link below is to a version of the Tetris game that I created using React. This project was a good one for me to experience, because it helped develop my sense of what can be achieved with only using some javascript and CSS. While seemingly a basic looking game, it was great to learn how to rotate the blocks through array/matrix manipulation! Use q & w to rotate!"
+            description="The link below is to a version of Tetris that I created using Javascript. This project was a good one for me to experience, because it helped develop a sense of what can be achieved with just javascript and CSS. While seemingly a basic looking game, it was great to learn how to rotate the blocks through array/matrix manipulation! Use q & w to rotate!"
             skills="JavaScript, Node.js/Express, CSS, HTML"
             url="https://javascript-tetris-mv.herokuapp.com/"
           />
